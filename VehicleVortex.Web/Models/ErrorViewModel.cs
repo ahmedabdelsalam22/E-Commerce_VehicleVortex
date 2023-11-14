653ddd3476972970ca1917acb56943e00b0a14ca
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-11-14 14:10:49 +0200

    add frontend proj

---
+++ VehicleVortex.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VehicleVortex.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
