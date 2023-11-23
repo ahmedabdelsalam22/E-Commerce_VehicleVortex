607db393a67b1bef099fbc7d752e37205184879e
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-11-23 02:14:05 +0200

    create authController and create login endPoint

---
+++ VehicleVortex/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VehicleVortex.Models;
+using VehicleVortex.Services.AuthServices.IAuthServices;
+
+namespace VehicleVortex.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _service;
+
+        public AuthController(IAuthService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<LoginResponseDTO>> Login([FromBody] LoginRequestDTO loginRequestDTO)
+        {
+            LoginResponseDTO loginResponse = await _service.Login(loginRequestDTO);
+            if (loginResponse.appUserDto == null || string.IsNullOrEmpty(loginResponse.Token))
+            {
+                return BadRequest(loginResponse);
+            }
+            return Ok(loginResponse);
+        }
+
+    }
+}
