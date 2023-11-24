30007d8f3d42abc2a83931fe242fb19a8116886c
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-11-24 03:01:43 +0200

    fix issue in signature key

--- VehicleVortex/Program.cs
+++ VehicleVortex/Program.cs
@@ -53,21 +53,7 @@ builder.Services.AddSwaggerGen(options => {
     
 });
 
-builder.Services.AddAuthentication(x => {
-    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
 
-}).AddJwtBearer(x => {
-    x.RequireHttpsMetadata = false;
-    x.SaveToken = true;
-    x.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        //IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes()),
-        ValidateIssuer = false,
-        ValidateAudience = false
-    };
-});
 
 
 
@@ -81,6 +67,26 @@ builder.Services.AddScoped<IAuthService, AuthService>();
 
 
 builder.Services.AddAutoMapper(typeof(MappingConfig));
+
+
+string? secretKey = builder.Configuration.GetValue<string>("JwtOptions:SecretKey");
+
+builder.Services.AddAuthentication(x => {
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+})
+    .AddJwtBearer(x => {
+        x.RequireHttpsMetadata = false;
+        x.SaveToken = true;
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey!)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
 
 var app = builder.Build();
 
