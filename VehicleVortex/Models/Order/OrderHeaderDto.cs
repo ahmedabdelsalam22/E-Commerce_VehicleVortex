5c980d253664f06481594e7fa08fa8a78cc52b58
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-11-27 19:46:16 +0200

    create order Dtos

---
+++ VehicleVortex/Models/Order/OrderHeaderDto.cs
@@ -0,0 +1,14 @@
+﻿namespace VehicleVortex.Models.Order
+{
+    public class OrderHeaderDto
+    {
+        public int OrderHeaderId { get; set; }
+        public string? UserId { get; set; }
+        public double OrderTotal { get; set; }
+        public DateTime OrderTime { get; set; }
+        public string? Status { get; set; }
+        public string? PaymentIntentId { get; set; }
+        public string? StripeSessionId { get; set; }
+        public IEnumerable<OrderDetails> OrderDetails { get; set; }
+    }
+}
